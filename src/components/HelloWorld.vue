aee7bc3eeea5385ce61f1d9709dd31e0eeb511bb
Daniel Lautzenheiser <lautzd@gmail.com>
2023-03-01 09:19:27 -0500

    Initial commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,48 @@
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import KeyboardHandler from "./KeyboardHandler.vue";
+
+@Component({ components: { KeyboardHandler } })
+export default class HelloWorld extends Vue {
+  keys = '';
+
+  updateKeys(keys: string[]) {
+    console.log('KEYS', keys)
+    this.keys = keys.join(' + ');
+  }
+}
+</script>
+
+<template>
+  <div class="greetings">
+    <h3>
+      {{ keys }}
+    </h3>
+    <KeyboardHandler @keydown="updateKeys" />
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    display: block;
+    text-align: left;
+  }
+}
+</style>
---
+++ src/components/KeyboardHandler.vue
@@ -0,0 +1,117 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+interface KeyEvents {
+  keys: string[];
+  eventName: string;
+}
+
+const staticKeys = [
+  "Meta",
+  "Control",
+  "Shift",
+  "Alt",
+  "F1",
+  "F2",
+  "F3",
+  "F4",
+  "F5",
+  "F6",
+  "F7",
+  "F8",
+  "F9",
+  "F10",
+  "F11",
+  "F12",
+];
+
+@Component({})
+export default class KeyboardHandler extends Vue {
+  keys: Record<string, boolean> = {};
+
+  handler: number | null = null;
+
+  @Prop({ type: Object, default: {} }) keyEvents!: KeyEvents[];
+
+  get optimizeEvents() {
+    return Math.random();
+  }
+
+  created() {
+    window.addEventListener("keydown", this.keydownHandler);
+    window.addEventListener("keyup", this.keyupHandler);
+  }
+
+  beforeDestroy() {
+    window.removeEventListener("keydown", this.keydownHandler);
+    window.removeEventListener("keyup", this.keyupHandler);
+  }
+
+  emitKeydownEvent() {
+    const finalKeys = Object.keys(this.keys ?? {});
+    finalKeys.sort((a, b) => {
+      const staticIndexA = staticKeys.indexOf(a);
+      const staticIndexB = staticKeys.indexOf(b);
+
+      if (staticIndexA !== -1 && staticIndexB !== -1) {
+        return staticIndexA - staticIndexB;
+      }
+
+      if (staticIndexA !== -1) {
+        return -1;
+      }
+
+      if (staticIndexB !== -1) {
+        return 1;
+      }
+
+      return a.localeCompare(b);
+    });
+
+    this.$emit("keydown", finalKeys);
+  }
+
+  getKey(e: KeyboardEvent): string {
+    return /^[a-zA-Z]{1}$/.test(e.key) ? e.key.toUpperCase() : e.key;
+  }
+
+  handleTimeout(): void {
+    if (this.handler) {
+      clearTimeout(this.handler);
+      this.handler = null;
+    }
+
+    this.handler = setTimeout(() => {
+      this.keys = {};
+      this.emitKeydownEvent();
+    }, 2000);
+  }
+
+  keydownHandler(e: KeyboardEvent) {
+    e.preventDefault();
+    if (e.key in this.keys) {
+      return;
+    }
+    this.keys[this.getKey(e)] = true;
+    this.emitKeydownEvent();
+    this.handleTimeout();
+  }
+
+  keyupHandler(e: KeyboardEvent) {
+    e.preventDefault();
+    delete this.keys[this.getKey(e)];
+    this.emitKeydownEvent();
+    this.handleTimeout();
+  }
+}
+</script>
+
+<style scoped>
+div {
+  display: none;
+}
+</style>
